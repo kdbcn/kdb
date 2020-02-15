f10ef5b1d9e51b7393066a2fa46b477ad2f16436
kdbcn <kdbcn@qq.com>
2020-02-15 20:31:33 +0800

    .

--- q.bat
+++ q.bat
@@ -1,12 +1,12 @@
 @echo off
 rem 切换到本文件所在盘符，如：  d:
 %~d0
-rem 进入本文件所在目录，如： cd d:\tick
+rem 进入本文件所在目录，如： cd d:\kdb
 cd %~dp0
 rem 设置QHOME，其中%~dp0 为当前批处理文件的路径。
 set QHOME=%~dp0q
 set SSL_VERIFY_SERVER=NO
 set KX_VERIFY_SERVER=NO
-start "5001" %QHOME%\w32\q.exe -p 5001 -U %QHOME%\qusers
+start "5001" %QHOME%\w32\q.exe -p 5001 
 
 
--- q64.bat
+++ q64.bat
@@ -1,12 +1,12 @@
 @echo off
 rem 切换到本文件所在盘符，如：  d:
 %~d0
-rem 进入本文件所在目录，如： cd d:\tick
+rem 进入本文件所在目录，如： cd d:\kdb
 cd %~dp0
 set SSL_VERIFY_SERVER=NO
 set KX_VERIFY_SERVER=NO
 rem 设置QHOME，其中%~dp0 为当前批处理文件的路径。
 set QHOME=%~dp0q
-start "5001" %QHOME%\w64\q.exe -p 5001 -U %QHOME%\qusers
+start "5002" %QHOME%\w64\q.exe -p 5002
 
 
